9ce6d1e7e55606e5cff74c2da18a5644f1f35cf8
Clément Prévost <prevostclement@gmail.com>
2016-06-10 00:41:07 +0200

    Add project search function

---
+++ server/migration/sqls/20160609222810-project-search-up.sql
@@ -0,0 +1,14 @@
+BEGIN;
+
+-- A procedure to search the headline and body of all posts using a given
+-- search term.
+create function give_me_time_public.project_search(search varchar) returns setof give_me_time_public.project as $$
+select *
+from give_me_time_public.project
+where title ilike ('%' || search || '%')
+      or description ilike ('%' || search || '%')
+$$ language sql stable set search_path from current;
+
+comment on function give_me_time_public.project_search(varchar) is 'Returns projects containing a given search term.';
+
+COMMIT;
