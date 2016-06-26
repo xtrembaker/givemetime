ed89dab4cdf62033e56d53d143497323a98298e8
Clément Prévost <prevostclement@gmail.com>
2016-06-26 23:40:00 +0200

    Add diffmonitor

--- src/components/DevTools.jsx
+++ src/components/DevTools.jsx
@@ -5,7 +5,7 @@ import { createDevTools } from 'redux-devtools'
 
 // Monitors are separate packages, and you can make a custom one
 import DockMonitor from 'redux-devtools-dock-monitor'
-import DiffMonitor from 'redux-devtools-diff-monitor';
+import DiffMonitor from 'redux-devtools-diff-monitor'
 
 // createDevTools takes a monitor and produces a DevTools component
 const DevTools = createDevTools(
