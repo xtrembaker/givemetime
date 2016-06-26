35f80a767dd78c30c550a07001f70416404ebbc9
Clément Prévost <prevostclement@gmail.com>
2016-06-26 23:39:10 +0200

    Add diffmonitor

--- src/components/DevTools.jsx
+++ src/components/DevTools.jsx
@@ -4,8 +4,8 @@ import React from 'react'
 import { createDevTools } from 'redux-devtools'
 
 // Monitors are separate packages, and you can make a custom one
-import LogMonitor from 'redux-devtools-log-monitor'
 import DockMonitor from 'redux-devtools-dock-monitor'
+import DiffMonitor from 'redux-devtools-diff-monitor';
 
 // createDevTools takes a monitor and produces a DevTools component
 const DevTools = createDevTools(
@@ -16,7 +16,7 @@ const DevTools = createDevTools(
     <DockMonitor toggleVisibilityKey='ctrl-h'
                  changePositionKey='ctrl-q'
                  defaultIsVisible={true}>
-        <LogMonitor theme='tomorrow' />
+        <DiffMonitor theme='tomorrow' />
     </DockMonitor>
 )
 
