5fcdf0b76997d364f2c91d5193dc214c4f9a3469
Clément Prévost <prevostclement@gmail.com>
2016-06-26 04:03:09 +0200

    Add psql script

--- scripts/_env.sh
+++ scripts/_env.sh
@@ -12,3 +12,6 @@ export PGPORT=5430
 export PGUSER=give_me_time
 export PGPASSWORD=give_me_time
 export PGDATABASE=give_me_time
+
+
+export HOST_PORT_FROM_CONTAINER=`docker exec -it server_db_1 sh -c "/sbin/ip route" | awk '/default/ { print $3 }' | sed '/^$/d'`
--- scripts/db-test.sh
+++ scripts/db-test.sh
@@ -3,5 +3,4 @@
 cd "$(dirname "$0")"
 source ./_env.sh
 
-HOST_PORT_FOM_CONTAINER=`docker exec -it server_db_1 sh -c "/sbin/ip route" | awk '/default/ { print $3 }' | sed '/^$/d'`
-docker run --rm -v $(pwd)/../server/migration/sqls/:/test digit/docker-pg-prove -h $HOST_PORT_FOM_CONTAINER -p $PGPORT -u $PGUSER -w $PGPASSWORD -d $PGDATABASE -t '/test/*-test.sql'
+docker run --rm -v $(pwd)/../server/migration/sqls/:/test digit/docker-pg-prove -h $HOST_PORT_FROM_CONTAINER -p $PGPORT -u $PGUSER -w $PGPASSWORD -d $PGDATABASE -t '/test/*-test.sql'
