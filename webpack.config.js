ad1a379d200f1faa90527fe7bbef709023ed665f
Clément Prévost <prevostclement@gmail.com>
2016-09-20 01:18:30 +0200

    Clean dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -1,60 +1,90 @@
-// From https://github.com/dwmkerr/react-es6-starter
+const merge = require('webpack-merge');
+const path = require('path');
+const webpack = require('webpack');
+const fs = require('fs');
 
-var path = require('path');
-var HtmlWebpackPlugin = require('html-webpack-plugin');
+const ENV = require('./env');
+const PATHS = {
+    src: path.join(__dirname, 'src'),
+    build: path.join(__dirname, 'build/public'),
+};
 
-module.exports = {
+process.env.BABEL_ENV = ENV;
 
-    //  Defines the entrypoint of our application.
-    entry: path.resolve(__dirname, './src/index.jsx'),
-
-    //  Bundle to a ./build/public/bundle.js file.
+const common = {
+    entry: PATHS.src,
     output: {
-        path: path.resolve(__dirname, './build/public'),
-        filename: 'bundle.js'
+        path: PATHS.build,
+        filename: 'bundle.js',
     },
-
-    //  Use babel for anything that is *.js or *.jsx.
     module: {
         loaders: [
             {
-                test: /\.js$/,
-                loader: 'babel-loader',
-                include: path.resolve(__dirname, './src')
+                test: /\.css$/,
+                loaders: ['style', 'css?url=false'],
+                //include: PATHS.src,
             },
             {
-                test: /\.jsx$/,
-                loader: 'babel-loader',
-                query: {
-                    presets: ['react']
-                },
-                include: path.resolve(__dirname, './src')
+                test: /\.jsx?$/,
+                loader: 'babel?cacheDirectory',
+                include: PATHS.src,
             }
         ]
     },
-
-    //  Configure the plugins. We copy the index.html
-    //  file to the build folder.
     plugins: [
-        new HtmlWebpackPlugin({
-            template: path.resolve(__dirname, './src/index.html'),
-            inject: 'body' // Inject webpack scripts into the body.
-        })
+        new webpack.DefinePlugin({
+            'process.env': {
+                'NODE_ENV': JSON.stringify(process.env.STAGING || 'development')
+            },
+        }),
     ],
-
-    // proxy graphql endpoint to our dev server at port 3000
-    devServer: {
-        proxy: {
-            "/graphql": {
-                "target": {
-                    "host": "localhost",
-                    "protocol": 'http:',
-                    "port": 3000
-                },
-                ignorePath: true,
-                changeOrigin: true,
-                secure: false
-            }
-        }
-    }
 };
+
+// enhance conf with development setup
+let conf = common;
+if (process.env.STAGING !== 'production') {
+    conf = merge(common, {
+        devtool: "#inline-source-map",
+        devServer: {
+            contentBase: PATHS.build,
+
+            // Enable history API fallback so HTML5 History API based
+            // routing works. This is a good default that will come
+            // in handy in more complicated setups.
+            historyApiFallback: {
+                index: '/',
+                rewrites: [
+                    {from: /.*\/bundle\.js.*/, to: '/bundle.js'},
+                    {from: /.*/, to: '/'},
+                ],
+            },
+            inline: true,
+            progress: true,
+
+            publicPath: '/',
+
+            // Display only errors to reduce the amount of output.
+            stats: 'errors-only',
+
+            // Parse host and port from env so this is easy to customize.
+            host: process.env.HOST,
+            port: process.env.PORT,
+
+            proxy: {
+                "/graphql": {
+                    "target": {
+                        "host": "localhost",
+                        "protocol": 'http:',
+                        "port": 3000
+                    },
+                    ignorePath: true,
+                    changeOrigin: true,
+                    secure: false
+                }
+            }
+        },
+    });
+}
+
+
+module.exports = conf;
