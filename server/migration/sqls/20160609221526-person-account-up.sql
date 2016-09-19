41b9236f5a74ab6901670f5dd1fdbc1d13f78b21
Clément Prévost <prevostclement@gmail.com>
2016-09-20 00:34:28 +0200

    Add email error messages

--- server/migration/sqls/20160609221526-person-account-up.sql
+++ server/migration/sqls/20160609221526-person-account-up.sql
@@ -2,7 +2,9 @@ BEGIN;
 
 create table give_me_time_private.person_account (
   person_id        integer not null primary key references give_me_time_public.person,
-  email            character varying not null unique check (email ~* '^.+@.+\..+$'),
+  email            character varying not null
+                   constraint email_unique unique
+                   constraint email_format check (email ~* '^.+@.+\..+$'),
   pass_hash        character varying not null
 );
 
@@ -10,5 +12,7 @@ comment on table give_me_time_private.person_account is 'Private information abo
 comment on column give_me_time_private.person_account.person_id is 'The id of the person associated with this account.';
 comment on column give_me_time_private.person_account.email is 'The email address of the person.';
 comment on column give_me_time_private.person_account.pass_hash is 'An opaque hash of the person’s password.';
+comment on constraint email_unique on give_me_time_private.person_account is 'Email already exists';
+comment on constraint email_format on give_me_time_private.person_account is 'Email format invalid';
 
 COMMIT;
